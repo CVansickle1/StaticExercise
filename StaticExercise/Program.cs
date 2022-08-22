f01a66371f59e996cc74a8a5acf95539ca09a49b
Cody <Cojo341@gmail.com>
2022-08-22 10:58:10 -0400

    Initial Commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+using StaticExercise;
+
+Console.WriteLine("Hello! Please enter the number you would like to convert from Fahrenheit to Celsius");
+
+var numCheck = double.TryParse(Console.ReadLine(), out var userFahrTemp);
+
+while(!numCheck)
+{
+    Console.WriteLine("Please enter a number: ");
+    numCheck = double.TryParse(Console.ReadLine(), out userFahrTemp);
+}
+
+Console.WriteLine($"{userFahrTemp} to Celsius is: ");
+Console.WriteLine($"{TempConverter.FahrenheitToCelsius(userFahrTemp)}°C");
+
+Console.WriteLine("------------------------------------");
+
+Console.WriteLine("Enter the number you would like to convert from Celsius to Fahrenheit");
+
+numCheck = double.TryParse(Console.ReadLine(), out var userCelsTemp);
+
+while(!numCheck)
+{
+    Console.WriteLine("Please enter a number: ");
+    numCheck = double.TryParse(Console.ReadLine(), out userCelsTemp);
+}
+
+
+Console.WriteLine($"{userCelsTemp} to Fahrenheit is: ");
+Console.WriteLine($"{TempConverter.CelsiusToFahrenheit(userCelsTemp)}°F");
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    internal class TempConverter
+    {
+
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            return Math.Round(((fahrenheit - 32) * .5556));
+        }
+
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            return Math.Round(((celsius * 1.8) + 32));
+        }
+
+    }
+}
